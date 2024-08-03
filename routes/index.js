1722a7f185f662f81f73c4d9aa0295604f96c64e
Fadi Asaad <firon1222@gmail.com>
2024-08-03 07:07:03 +0300

    add: POST /users endpoint. related to #13

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import { status, stats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
 
 const router = Router();
 
+// check stats and status
 router.get('/status', status);
 router.get('/stats', stats);
+
+// users
+router.post('/users', postNew);
 
 export default router;
