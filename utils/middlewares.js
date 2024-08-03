d6c0188abbd1003ce1ce185870f207beea1a086a
Fadi Asaad <firon1222@gmail.com>
2024-08-03 17:17:57 +0300

    update: json and error responses middlewares to auto set status to 400 for errors and 200 for OK responses

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -2,12 +2,14 @@
  * An express middleware that provides `res.json` that responses with a json
  * data in the body of the response.
  * Also sets the `Content-Type` header to `application/json`.
+ *
+ * prototype `res.json(obj: object, status: number = 200)`
  */
 export const jsonResponseMiddleware = (_, res, next) => {
-  res.json = (obj) => {
+  res.json = (obj, status = 200) => {
     const data = JSON.stringify(obj);
     res.set('Content-Type', 'application/json');
-    res.send(data);
+    res.status(status).send(data);
   };
 
   next();
@@ -17,9 +19,11 @@ export const jsonResponseMiddleware = (_, res, next) => {
  * An express middleware that provides `res.error` that responses with a json
  * error structure.
  *
- * Example: `{ "error": "error message..." }`
+ * prototype `res.error(error: string, status: number = 400)`
+ *
+ * Example error object: `{ "error": "error message..." }`
  */
 export const errorResponseMiddleware = (_, res, next) => {
-  res.error = (error) => res.json({ error });
+  res.error = (error, status = 400) => res.json({ error }, status);
   next();
 };
