7c6fc7e7092a1b4370db7ad5ceb55876a106c72d
Fadi Asaad <firon1222@gmail.com>
2024-08-03 17:33:29 +0300

    update: use UsersController as a class with postNew as a static function

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,32 +2,39 @@ import { hashPassword } from '../utils/password_hashing';
 import dbClient from '../utils/db';
 
 /**
- * A route controller to add a new user
+ * A controller for the users endpoints
  */
-export default async function postNew(req, res) {
-  const { email, password } = req.body;
+class UsersController {
+  /**
+   * A route controller to add a new user
+   */
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-  if (!email) {
-    return res.error('Missing email');
+    if (!email) {
+      return res.error('Missing email');
+    }
+
+    if (!password) {
+      return res.error('Missing password');
+    }
+
+    // check if a user with the same email is already exist
+    const foundUser = await dbClient.users.findOne({ email });
+    if (foundUser) {
+      return res.error('Already exist');
+    }
+
+    const hashedPassword = hashPassword(password);
+
+    const results = await dbClient.users.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    const user = results.ops[0];
+    return res.json({ id: user._id, email: user.email }, 201);
   }
-
-  if (!password) {
-    return res.error('Missing password');
-  }
-
-  // check if a user with the same email is already exist
-  const foundUser = await dbClient.users.findOne({ email });
-  if (foundUser) {
-    return res.error('Already exist');
-  }
-
-  const hashedPassword = hashPassword(password);
-
-  const results = await dbClient.users.insertOne({
-    email,
-    password: hashedPassword,
-  });
-
-  const user = results.ops[0];
-  return res.json({ id: user._id, email: user.email }, 201);
 }
+
+export default UsersController;
