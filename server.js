3bf491f1be3be24b8feecc6b01fa32df2ad973f2
Fadi Asaad <firon1222@gmail.com>
2024-08-03 07:05:29 +0300

    add: express middleware to ease json and error responding

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/index';
+import {
+  jsonResponseMiddleware,
+  errorResponseMiddleware,
+} from './utils/middlewares';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(jsonResponseMiddleware);
+app.use(errorResponseMiddleware);
 app.use(router);
 
 app.listen(port, () => {
