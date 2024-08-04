3b3d18b53773a814b622866f61e984b5a3b2a348
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-04 22:39:28 +0300

    add auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -10,5 +11,10 @@ router.get('/stats', getStats);
 
 // users
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// auth
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.disConnect);
 
 export default router;
