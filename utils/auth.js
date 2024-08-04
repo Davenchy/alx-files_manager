c9bc786a97cc016864a654b5ecf9e3edd87db4a4
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-08-04 22:39:56 +0300

    add helper function for auth controllers

---
+++ utils/auth.js
@@ -0,0 +1,14 @@
+import dbClient from './db';
+import { hashPassword } from './password_hashing';
+
+async function isUserExists(email, password) {
+  const hashedPassword = hashPassword(password);
+  const user = await dbClient.users.findOne({ email, password: hashedPassword });
+  console.log(email, hashedPassword);
+  if (!user) {
+    return false;
+  }
+  return user._id.toString();
+}
+
+export default isUserExists;
