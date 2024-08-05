11269ab9a1c28bfe50084f60358a9906bbbbae2d
Fadi Asaad <firon1222@gmail.com>
2024-08-05 12:28:35 +0300

    clean: unneeded imports at UsersController and console.log at auth utility

--- utils/auth.js
+++ utils/auth.js
@@ -3,8 +3,10 @@ import { hashPassword } from './password_hashing';
 
 async function isUserExists(email, password) {
   const hashedPassword = hashPassword(password);
-  const user = await dbClient.users.findOne({ email, password: hashedPassword });
-  console.log(email, hashedPassword);
+  const user = await dbClient.users.findOne({
+    email,
+    password: hashedPassword,
+  });
   if (!user) {
     return false;
   }
